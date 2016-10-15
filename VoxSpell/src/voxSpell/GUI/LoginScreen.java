18b0f8ea8d32213569e0f21348de3f8ee1bfecbd
nathangavin <ngav018@aucklanduni.ac.nz>
2016-10-15 19:52:05 +1300

    more progress

---
+++ VoxSpell/src/voxSpell/GUI/LoginScreen.java
@@ -0,0 +1,20 @@
+package voxSpell.GUI;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+
+public class LoginScreen extends GUIElement {
+
+	protected LoginScreen(JFrame frame) {
+		super(frame);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
