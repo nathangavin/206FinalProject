2c69fc0e7c25f991d3ffcdc95d543baed31e4d8c
nathangavin <ngav018@aucklanduni.ac.nz>
2016-10-15 14:20:50 +1300

    beginning the project structure

---
+++ VoxSpell/src/voxSpell/data/Topic.java
@@ -0,0 +1,10 @@
+package voxSpell.data;
+
+import java.util.ArrayList;
+
+public class Topic {
+
+	private String _name = "";
+	private ArrayList<WordList> _wordlists = new ArrayList<WordList>();
+	
+}
